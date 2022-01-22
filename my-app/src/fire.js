233c07a1375f336d30cfa754d0ee68a4ff60d784
Sebastian <61755980+SebastianD101@users.noreply.github.com>
2022-01-22 12:41:10 -0500

    fixed firebase bug p2

--- my-app/src/fire.js
+++ my-app/src/fire.js
@@ -1,18 +1,24 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCw4Ss5jkVufOA9f4ayJJmavDQ4TnMwkVw",
-    authDomain: "boilernotes-61e36.firebaseapp.com",
-    projectId: "boilernotes-61e36",
-    storageBucket: "boilernotes-61e36.appspot.com",
-    messagingSenderId: "4786131586",
-    appId: "1:4786131586:web:7dd25e336ba5c2725f9a9b",
-    measurementId: "G-L03ZVSMW9Y"
+  apiKey: "AIzaSyCw4Ss5jkVufOA9f4ayJJmavDQ4TnMwkVw",
+  authDomain: "boilernotes-61e36.firebaseapp.com",
+  databaseURL: "https://boilernotes-61e36-default-rtdb.firebaseio.com",
+  projectId: "boilernotes-61e36",
+  storageBucket: "boilernotes-61e36.appspot.com",
+  messagingSenderId: "4786131586",
+  appId: "1:4786131586:web:7dd25e336ba5c2725f9a9b",
+  measurementId: "G-L03ZVSMW9Y"
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default db;
